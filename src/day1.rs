99dec9a7e23c4b12fbb581d62d112f754da0335b
Benjamin Weber <mail@bwe.im>
2019-12-01 21:20:34 +0100

    add day 1.1

--- src/day1.rs
+++ src/day1.rs
@@ -17,17 +17,25 @@ impl Module {
         mass_to_fuel(self.mass)
     }
 
-    pub fn total_fuel(self) -> Fuel {
+    pub fn total_fuel_incl_additional(self) -> Fuel {
         std::iter::successors(Some(self.launch_fuel()), |&fuel| Some(mass_to_fuel(fuel)))
             .take_while(|&fuel| fuel != 0)
             .sum::<Fuel>()
     }
 }
 
-pub fn total_fuel_of_modules(content: &'static str) -> Fuel {
+pub fn total_basic_fuel_of_modules(content: &'static str) -> Fuel {
     content
         .lines()
         .filter_map(|x| x.parse().ok())
-        .map(|mass| Module { mass }.total_fuel())
+        .map(|mass| Module { mass }.launch_fuel())
+        .sum::<Fuel>()
+}
+
+pub fn total_fuel_of_modules_incl_additional(content: &'static str) -> Fuel {
+    content
+        .lines()
+        .filter_map(|x| x.parse().ok())
+        .map(|mass| Module { mass }.total_fuel_incl_additional())
         .sum::<Fuel>()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod day1;
 
 fn main() {
     let day1 = include_str!("../inputs/day1.txt");
-    println!("Day 1: Total fuel required: {}", day1::total_fuel_of_modules(&day1))
+    println!("Day 1.1: Total fuel required for space ship alone: {}", day1::total_basic_fuel_of_modules(&day1));
+    println!("Day 1.2: Total fuel required, including fuel for fuel: {}", day1::total_fuel_of_modules_incl_additional(&day1));
 }
