406c264b99d3ffd324c6682c6a9f3ab84dfff707
Benjamin Weber <mail@bwe.im>
2019-12-01 22:40:41 +0100

    delete main.rs; create boilerplate lib.rs; put tests into day1.rs

--- src/day1.rs
+++ src/day1.rs
@@ -39,3 +39,20 @@ pub fn total_fuel_of_modules_incl_additional(content: &'static str) -> Fuel {
         .map(|mass| Module { mass }.total_fuel_incl_additional())
         .sum::<Fuel>()
 }
+
+
+mod tests {
+    use crate::day1::*;
+    #[test]
+    /// Day 1.1: Total fuel required for space ship alone
+    fn day1_1() {
+        let d1 = include_str!("../inputs/day1.txt");
+        assert_eq!(total_basic_fuel_of_modules(&d1), 3_478_233);
+    }
+    /// Day 1.2: Total fuel required, including fuel for fuel
+    #[test]
+    fn day1_2() {
+        let d1 = include_str!("../inputs/day1.txt");
+        assert_eq!(total_fuel_of_modules_incl_additional(&d1), 5_214_475);
+    }
+}
